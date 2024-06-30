a8f0b22722bf147bb72245b13f7dfa65390ca037
invincible-demon <123808875+invincible-demon@users.noreply.github.com>
2024-06-30 11:22:21 +0530

    Update OpenWeatherService.js

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 86ef1237991696f7fccc8303cd6c667d;
+const WEATHER_API_KEY = '86ef1237991696f7fccc8303cd6c667d';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
